6c0bc4d963f9c4ba518cde37cae163ce7bbb171d
yseul <qlcid@naver.com>
2020-03-02 17:30:10 +0900

    Using Props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Subject from './components/Subject';
+import Topic from './components/Topic';
+import Content from './components/Content';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="world wide web!" />
+        <Topic />
+        <Content title="HTML" sub="HTML is HyperText Markup Language." />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Topic.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Topic extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.CSS">CSS</a></li>
+          <li><a href="3.Javascript">Javascript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Topic;
