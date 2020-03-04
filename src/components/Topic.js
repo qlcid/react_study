499210b8063a378f939807bbd39dcadcbb361521
yseul <qlcid@naver.com>
2020-03-04 18:33:07 +0900

    Setting Event Handling

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 
 class Topic extends Component {
   render() {
+    var Lists = [];
+    var data = this.props.data;
+    var i = 0;
+    while(i < data.length) {
+      Lists.push(<li key={data[i].id}>
+        <a href={"/content/" + data[i].id} data-id={data[i].id} onClick={function(e) {
+            e.preventDefault();
+            this.props.onChangePage(e.target.dataset.id);
+        }.bind(this)}>
+          {data[i].title}
+        </a>
+      </li>);
+      i = i + 1;
+    }
+
     return (
       <nav>
         <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.CSS">CSS</a></li>
-          <li><a href="3.Javascript">Javascript</a></li>
+          {Lists}
         </ul>
       </nav>
     );
